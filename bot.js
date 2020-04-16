ca3a979ed5ee76a06218b4c4935e5bcf318203f3
jfa <j.faehser+github@gmail.com>
2020-04-16 12:59:36 +0200

    added second bot command

--- bot.js
+++ bot.js
@@ -32,6 +32,11 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     to: channelID,
                     message: 'Pong!'
                 });
+            case '6':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'high five'
+                })
             break;
             // Just add any case commands if you want to..
          }
