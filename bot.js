e9f96f867b86592197264ee58681083ab210a7c7
jfa <j.faehser+github@gmail.com>
2020-04-16 21:09:07 +0200

    added gitignore

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 var Discord = require('discord.io');
 var logger = require('winston');
-var auth = require('./.gitignore/auth.json');
+var auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
